2d69ca9344caf4ddff17bf8cba57eb1e38d6680b
igeofr <Florian@iMac-de-Florian.local>
2015-02-26 12:11:53 +0100

    Update

--- python/csv_rgb_to_qml.py
+++ python/csv_rgb_to_qml.py
@@ -19,17 +19,20 @@ read_csv.next()
 ########Creation d'un tableau
 tab = []
 for row in read_csv:
+    
 ######## Permet de definir l'encodage du csv
         unicode_row = [x.decode('latin1') for x in row]
+        
 ######## Permet de definir les colonnes value, label, red, green, blue dans le csv
         col_select =row[0], row[1],row[4], row[5], row[6]
         tab.append(col_select)
         
 categories = []
 for value, label, red, green, blue in tab :
-    #Concatener r,g,b
+    
+########Concatener r,g,b
     color_rgb = red+','+green+','+blue
-    #print color_rgb
+    
 ######## Source : http://gis.stackexchange.com/questions/53121/how-change-border-line-to-no-pen-with-python-console
     symbol = QgsFillSymbolV2.createSimple( {'style':'solid','outline_style':'no','outline_width':'0','color':color_rgb} )
     symbol.setAlpha (0.5)
@@ -45,8 +48,8 @@ if layer <> None:
 
     ######## Permet d'exporter le qml
     layer.saveNamedStyle('/Users/Florian/Downloads/style.qml')
+    
 else :
     iface.messageBar().pushMessage("Attention", "Selectionner une couche", level=QgsMessageBar.WARNING, duration=2)
-    #print 'Attention : selectionner une couche'
 
 iface.mapCanvas().refresh() 
