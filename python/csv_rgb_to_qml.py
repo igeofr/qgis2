78ef6749750e34967b0613a8a4538f99734b41f2
igeofr <Florian@iMac-de-Florian.local>
2015-02-26 13:06:00 +0100

    Encodage

--- python/csv_rgb_to_qml.py
+++ python/csv_rgb_to_qml.py
@@ -2,16 +2,17 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from qgis.gui import QgsMessageBar
 import csv
+import codecs
 
 layer = iface.activeLayer()
 
 ######## Emplacement du csv
-InFlnm='CLC_DOM_nomenclature_03.csv'
+InFlnm='CLC_DOM_nomenclature_03b.csv'
 InDrPth='/Users/Florian/Downloads/'
 InFlPth=InDrPth+InFlnm
 
 ######## Ouverture du csv
-read_csv = csv.reader(open(InFlPth,"rU"),delimiter=";")
+read_csv = csv.reader(codecs.open(InFlPth,"rU", "utf-8"),delimiter=";")
 
 ########Permet de passer l'entete du csv
 read_csv.next()
@@ -19,14 +20,10 @@ read_csv.next()
 ########Creation d'un tableau
 tab = []
 for row in read_csv:
-    
-######## Permet de definir l'encodage du csv
-    unicode_row = [x.decode('latin1') for x in row]
-        
+
 ######## Permet de definir les colonnes value, label, red, green, blue dans le csv
-    col_select =row[0], row[1],row[4], row[5], row[6]
-    print col_select
-    tab.append(col_select)
+        col_select =row[0], row[1],row[4], row[5], row[6]
+        tab.append(col_select)
         
 categories = []
 for value, label, red, green, blue in tab :
