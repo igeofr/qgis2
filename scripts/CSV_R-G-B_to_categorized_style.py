e5873c7275bb35bed328f471a5f3544bc5ce92ad
igeofr <Florian@iMac-de-Florian.local>
2015-02-27 23:24:33 +0100

    Update

--- scripts/CSV_R-G-B_to_categorized_style.py
+++ scripts/CSV_R-G-B_to_categorized_style.py
@@ -41,70 +41,75 @@ if  fileExtension =='.csv':
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     #Creation d'un tableau qui va stocker les lignes et colonnes choisies du CSV  
     tab = []
-    for row in read_csv:
+    if Colonne_de_la_value >= 0 and  Colonne_du_label>= 0 and Colonne_red >= 0 and Colonne_green >= 0 and Colonne_blue >= 0 and Transparence_du_style>0 and Transparence_du_style<1 and Line_Outline_width >0:
+        for row in read_csv:
             # Permet de definir les colonnes value, label, red, green, blue
             col_select =row[Colonne_de_la_value], row[Colonne_du_label],row[Colonne_red], row[Colonne_green], row[Colonne_blue]
             # Insere chaque ligne du CSV dans le tableau
             tab.append(col_select)
+   
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-    categories = []
-    for value, label, red, green, blue in tab :
-        #Concatener r,g,b
-        color_rgb = red+','+green+','+blue
-        tab_list = value +' - '+label.decode(Encodage_du_CSV)+' - '+red+','+green+','+blue
-        progress.setText(u'Valeurs : %s' % tab_list)
-        # Creation de la ligne
-        if Outline == False :
-           b_outline = 'no'
-        else :
-            b_outline = 'yes'
-        # Largeur de la ligne
-        v_width = str(Line_Outline_width)
+        categories = []
+        for value, label, red, green, blue in tab :
+            #Concatener r,g,b
+            color_rgb = red+','+green+','+blue
+            tab_list = value +' - '+label.decode(Encodage_du_CSV)+' - '+red+','+green+','+blue
+            progress.setText(u'Valeurs : %s' % tab_list)
+            # Creation de la ligne
+            if Outline == False :
+               b_outline = 'no'
+            else :
+                b_outline = 'yes'
+            # Largeur de la ligne
+            v_width = str(Line_Outline_width)
 
-        # Polygones
-        if layer.dataProvider().geometryType() == 3:
-            # Source : http://gis.stackexchange.com/questions/53121/how-change-border-line-to-no-pen-with-python-console
-            symbol = QgsFillSymbolV2.createSimple( {'style':'solid','outline_style':b_outline,'outline_width':v_width,'color':color_rgb} )
-            symbol.setAlpha (Transparence_du_style)
-                
-            category = QgsRendererCategoryV2(value, symbol, label)
-            categories.append(category)
+            # Polygones
+            if layer.dataProvider().geometryType() == 3:
+                # Source : http://gis.stackexchange.com/questions/53121/how-change-border-line-to-no-pen-with-python-console
+                symbol = QgsFillSymbolV2.createSimple( {'style':'solid','outline_style':b_outline,'outline_width':v_width,'color':color_rgb} )
+                symbol.setAlpha (Transparence_du_style)
+                    
+                category = QgsRendererCategoryV2(value, symbol, label)
+                categories.append(category)
 
-        # Lignes
-        if layer.dataProvider().geometryType() == 2:     
-            symbol = QgsLineSymbolV2.createSimple( {'style':'solid','line_width':v_width,'color': color_rgb} )
-            symbol.setAlpha (Transparence_du_style)
-                
-            category = QgsRendererCategoryV2(value, symbol, label)
-            categories.append(category)
+            # Lignes
+            if layer.dataProvider().geometryType() == 2:     
+                symbol = QgsLineSymbolV2.createSimple( {'style':'solid','line_width':v_width,'color': color_rgb} )
+                symbol.setAlpha (Transparence_du_style)
+                    
+                category = QgsRendererCategoryV2(value, symbol, label)
+                categories.append(category)
 
-        # Points
-        if layer.dataProvider().geometryType() == 1:     
-            symbol = QgsMarkerSymbolV2.createSimple( {'style':'solid','outline_style':'no','outline_width':v_width,'color': color_rgb} )
-            symbol.setAlpha (Transparence_du_style)
-                
-            category = QgsRendererCategoryV2(value, symbol, label)
-            categories.append(category)
+            # Points
+            if layer.dataProvider().geometryType() == 1:     
+                symbol = QgsMarkerSymbolV2.createSimple( {'style':'solid','outline_style':'no','outline_width':v_width,'color': color_rgb} )
+                symbol.setAlpha (Transparence_du_style)
+                    
+                category = QgsRendererCategoryV2(value, symbol, label)
+                categories.append(category)
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-# Permet de creer le rendu et de l'affecter a la couche sur un champ defini
-    
-    # Nom du champ sur lequel doit s'appliquer la symbologie
-    expression = Nom_du_champ
-
-    renderer = QgsCategorizedSymbolRendererV2(expression, categories)
-    layer.setRendererV2(renderer)
-    
-    # Creation des fichiers de style
-    if Save_layer_style_as_default :
-        # QML
-        layer.saveDefaultStyle() 
-        # SLD
-        layer.saveSldStyle(myDirectory+'/'+nomCouche+'.sld')
+    # Permet de creer le rendu et de l'affecter a la couche sur un champ defini
         
-        progress.setText(u'Creation du fichier QML et SLD')
-            
-    iface.messageBar().pushMessage("Notification :", "C'est gagne, c'est gagne!", QgsMessageBar.INFO, duration=5)
-    iface.mapCanvas().refresh() 
+        # Nom du champ sur lequel doit s'appliquer la symbologie
+        expression = Nom_du_champ
 
+        renderer = QgsCategorizedSymbolRendererV2(expression, categories)
+        layer.setRendererV2(renderer)
+        
+        # Creation des fichiers de style
+        if Save_layer_style_as_default :
+            # QML
+            layer.saveDefaultStyle() 
+            # SLD
+            layer.saveSldStyle(myDirectory+'/'+nomCouche+'.sld')
+            
+            progress.setText(u'Creation du fichier QML et SLD')
+                
+        iface.messageBar().pushMessage("Notification :", "C'est gagne, c'est gagne!", QgsMessageBar.INFO, duration=5)
+        
+        iface.mapCanvas().refresh() 
+    else :
+        iface.messageBar().pushMessage("Ohoh :", "Probleme de valeur", QgsMessageBar.WARNING, duration=15)
+        
 else :
     iface.messageBar().pushMessage("Ohoh :", "J'ai demande un csv!", QgsMessageBar.WARNING, duration=15)
