a9b19413264a779a0ae6c56e2fe09b4a3c98728e
igeofr <Florian@iMac-de-Florian.local>
2016-03-23 22:58:11 +0100

    Count & Help

--- scripts/Import_color_ramp_Colourlovers.com.py
+++ scripts/Import_color_ramp_Colourlovers.com.py
@@ -2,7 +2,7 @@
 ##Import color ramp - ColourLovers.com=name
 
 ##URL_palette_ColourLovers=string 
-##Nom_du_style_en_sortie=string
+##Style_name=string
 ##Discrete=boolean True
 
 from qgis.core import QgsGradientStop,QgsVectorGradientColorRampV2,QgsStyleV2
@@ -14,7 +14,7 @@ from string import split
 
 tab = []
 
-if Nom_du_style_en_sortie <> "":
+if Style_name <> "":
     if  URL_palette_ColourLovers[0:36]== 'http://www.colourlovers.com/palette/':
 
         req = urllib2.Request(URL_palette_ColourLovers, headers={'User-Agent' : "Mozilla/5.0 "})
@@ -56,42 +56,125 @@ if Nom_du_style_en_sortie <> "":
                             progress.setText(u'Couleur : %s' % data)
                             tab.append(data)
 
-
             parser = AllColor()
             parser.feed(htmlSource)
+                       
+            if len(tab) == 2 :
 
-            if Discrete ==True :
+                if Discrete ==True :
+                    stop1 = QgsGradientStop(0.5, QColor(int(split(tab[1],',')[0]), int(split(tab[1],',')[1]), int(split(tab[1],',')[2])))
+                    stops = [stop1]
+                    discrete = True
+                    
+                    color1 = QColor(int(split(tab[0],',')[0]), int(split(tab[0],',')[1]), int(split(tab[0],',')[2]))
+                    color2 = QColor(int(split(tab[1],',')[0]), int(split(tab[1],',')[1]), int(split(tab[1],',')[2]))
+                    colorRamp = QgsVectorGradientColorRampV2(color1, color2, discrete,stops)
+                    
+                    myStyle = QgsStyleV2().defaultStyle()
+                    myStyle.addColorRamp(Style_name, colorRamp,True)
+                    
+                else:
+                    discrete = False
+                    
+                    color1 = QColor(int(split(tab[0],',')[0]), int(split(tab[0],',')[1]), int(split(tab[0],',')[2]))
+                    color2 = QColor(int(split(tab[1],',')[0]), int(split(tab[1],',')[1]), int(split(tab[1],',')[2]))
+                    colorRamp = QgsVectorGradientColorRampV2(color1, color2, discrete)
+                    
+                    myStyle = QgsStyleV2().defaultStyle()
+                    myStyle.addColorRamp(Style_name, colorRamp,True)   
 
-                stop1 = QgsGradientStop(0.2, QColor(int(split(tab[1],',')[0]), int(split(tab[1],',')[1]), int(split(tab[1],',')[2])))
-                stop2 = QgsGradientStop(0.4, QColor(int(split(tab[2],',')[0]), int(split(tab[2],',')[1]), int(split(tab[2],',')[2])))
-                stop3 = QgsGradientStop(0.6, QColor(int(split(tab[3],',')[0]), int(split(tab[3],',')[1]), int(split(tab[3],',')[2])))
-                stop4 = QgsGradientStop(0.8, QColor(int(split(tab[4],',')[0]), int(split(tab[4],',')[1]), int(split(tab[4],',')[2])))
-                stops = [stop1,stop2,stop3,stop4]
-                discrete = True
-                
-                color1 = QColor(int(split(tab[0],',')[0]), int(split(tab[0],',')[1]), int(split(tab[0],',')[2]))
-                color2 = QColor(int(split(tab[4],',')[0]), int(split(tab[4],',')[1]), int(split(tab[4],',')[2]))
+            if len(tab) == 3 :
 
-                colorRamp = QgsVectorGradientColorRampV2(color1, color2, discrete, stops)
-                
-                myStyle = QgsStyleV2().defaultStyle()
-                myStyle.addColorRamp(Nom_du_style_en_sortie, colorRamp)
-                
+                if Discrete ==True :
+                    stop1 = QgsGradientStop(0.33, QColor(int(split(tab[1],',')[0]), int(split(tab[1],',')[1]), int(split(tab[1],',')[2])))
+                    stop2 = QgsGradientStop(0.66, QColor(int(split(tab[2],',')[0]), int(split(tab[2],',')[1]), int(split(tab[2],',')[2])))
+                    stops = [stop1,stop2]
+                    discrete = True
+                    
+                    color1 = QColor(int(split(tab[0],',')[0]), int(split(tab[0],',')[1]), int(split(tab[0],',')[2]))
+                    color2 = QColor(int(split(tab[2],',')[0]), int(split(tab[2],',')[1]), int(split(tab[2],',')[2]))
+                    colorRamp = QgsVectorGradientColorRampV2(color1, color2, discrete,stops)
+                    
+                    myStyle = QgsStyleV2().defaultStyle()
+                    myStyle.addColorRamp(Style_name, colorRamp,True)
+                    
+                else:
+                    stop1 = QgsGradientStop(0.5, QColor(int(split(tab[1],',')[0]), int(split(tab[1],',')[1]), int(split(tab[1],',')[2])))
+                    stops = [stop1]
+                    discrete = False
+                    
+                    color1 = QColor(int(split(tab[0],',')[0]), int(split(tab[0],',')[1]), int(split(tab[0],',')[2]))
+                    color2 = QColor(int(split(tab[2],',')[0]), int(split(tab[2],',')[1]), int(split(tab[2],',')[2]))
+                    colorRamp = QgsVectorGradientColorRampV2(color1, color2, discrete,stops)
+                    
+                    myStyle = QgsStyleV2().defaultStyle()
+                    myStyle.addColorRamp(Style_name, colorRamp,True)                   
 
-            else:
-                stop1 = QgsGradientStop(0.25, QColor(int(split(tab[1],',')[0]), int(split(tab[1],',')[1]), int(split(tab[1],',')[2])))
-                stop2 = QgsGradientStop(0.50, QColor(int(split(tab[2],',')[0]), int(split(tab[2],',')[1]), int(split(tab[2],',')[2])))
-                stop3 = QgsGradientStop(0.75, QColor(int(split(tab[3],',')[0]), int(split(tab[3],',')[1]), int(split(tab[3],',')[2])))
-                stops = [stop1,stop2,stop3]
-                discrete = False
-                
-                color1 = QColor(int(split(tab[0],',')[0]), int(split(tab[0],',')[1]), int(split(tab[0],',')[2]))
-                color2 = QColor(int(split(tab[4],',')[0]), int(split(tab[4],',')[1]), int(split(tab[4],',')[2]))
+            if len(tab) == 4 :
 
-                colorRamp = QgsVectorGradientColorRampV2(color1, color2, discrete, stops)
+                if Discrete ==True :
+                    stop1 = QgsGradientStop(0.25, QColor(int(split(tab[1],',')[0]), int(split(tab[1],',')[1]), int(split(tab[1],',')[2])))
+                    stop2 = QgsGradientStop(0.50, QColor(int(split(tab[2],',')[0]), int(split(tab[2],',')[1]), int(split(tab[2],',')[2])))
+                    stop3 = QgsGradientStop(0.75, QColor(int(split(tab[3],',')[0]), int(split(tab[3],',')[1]), int(split(tab[3],',')[2])))
+                    stops = [stop1,stop2,stop3]
+                    discrete = True
+                    
+                    color1 = QColor(int(split(tab[0],',')[0]), int(split(tab[0],',')[1]), int(split(tab[0],',')[2]))
+                    color2 = QColor(int(split(tab[3],',')[0]), int(split(tab[3],',')[1]), int(split(tab[3],',')[2]))
+                    colorRamp = QgsVectorGradientColorRampV2(color1, color2, discrete,stops)
+                    
+                    myStyle = QgsStyleV2().defaultStyle()
+                    myStyle.addColorRamp(Style_name, colorRamp,True)
+                    
+                else:
+                    stop1 = QgsGradientStop(0.33, QColor(int(split(tab[1],',')[0]), int(split(tab[1],',')[1]), int(split(tab[1],',')[2])))
+                    stop2 = QgsGradientStop(0.66, QColor(int(split(tab[2],',')[0]), int(split(tab[2],',')[1]), int(split(tab[2],',')[2])))
+                    stops = [stop1,stop2]
+                    discrete = False
+                    
+                    color1 = QColor(int(split(tab[0],',')[0]), int(split(tab[0],',')[1]), int(split(tab[0],',')[2]))
+                    color2 = QColor(int(split(tab[3],',')[0]), int(split(tab[3],',')[1]), int(split(tab[3],',')[2]))
+                    colorRamp = QgsVectorGradientColorRampV2(color1, color2, discrete,stops)
+                    
+                    myStyle = QgsStyleV2().defaultStyle()
+                    myStyle.addColorRamp(Style_name, colorRamp,True)   
+
+            if len(tab)==5 :                 
                 
-                myStyle = QgsStyleV2().defaultStyle()
-                myStyle.addColorRamp(Nom_du_style_en_sortie, colorRamp)
+                if Discrete ==True :
+
+                    stop1 = QgsGradientStop(0.2, QColor(int(split(tab[1],',')[0]), int(split(tab[1],',')[1]), int(split(tab[1],',')[2])))
+                    stop2 = QgsGradientStop(0.4, QColor(int(split(tab[2],',')[0]), int(split(tab[2],',')[1]), int(split(tab[2],',')[2])))
+                    stop3 = QgsGradientStop(0.6, QColor(int(split(tab[3],',')[0]), int(split(tab[3],',')[1]), int(split(tab[3],',')[2])))
+                    stop4 = QgsGradientStop(0.8, QColor(int(split(tab[4],',')[0]), int(split(tab[4],',')[1]), int(split(tab[4],',')[2])))
+                    stops = [stop1,stop2,stop3,stop4]
+                    discrete = True
+                    
+                    color1 = QColor(int(split(tab[0],',')[0]), int(split(tab[0],',')[1]), int(split(tab[0],',')[2]))
+                    color2 = QColor(int(split(tab[4],',')[0]), int(split(tab[4],',')[1]), int(split(tab[4],',')[2]))
+
+                    colorRamp = QgsVectorGradientColorRampV2(color1, color2, discrete, stops)
+                    
+                    myStyle = QgsStyleV2().defaultStyle()
+                    myStyle.addColorRamp(Style_name, colorRamp,True)
+                    
+                    
+                else:
+                    stop1 = QgsGradientStop(0.25, QColor(int(split(tab[1],',')[0]), int(split(tab[1],',')[1]), int(split(tab[1],',')[2])))
+                    stop2 = QgsGradientStop(0.50, QColor(int(split(tab[2],',')[0]), int(split(tab[2],',')[1]), int(split(tab[2],',')[2])))
+                    stop3 = QgsGradientStop(0.75, QColor(int(split(tab[3],',')[0]), int(split(tab[3],',')[1]), int(split(tab[3],',')[2])))
+                    stops = [stop1,stop2,stop3]
+                    discrete = False
+                    
+                    color1 = QColor(int(split(tab[0],',')[0]), int(split(tab[0],',')[1]), int(split(tab[0],',')[2]))
+                    color2 = QColor(int(split(tab[4],',')[0]), int(split(tab[4],',')[1]), int(split(tab[4],',')[2]))
+
+                    colorRamp = QgsVectorGradientColorRampV2(color1, color2, discrete, stops)
+                    
+                    myStyle = QgsStyleV2().defaultStyle()
+                    myStyle.addColorRamp(Style_name, colorRamp,True)
+                    
+
 
     else :
         progress.setText(u'Warning : URL!')
